cd42d1f2cc4446a01de16aa0de31ebac376a6d3a
Krzysztof <krzysiekk.gda@gmail.com>
2018-02-08 23:08:39 +0000

    populate Models select based on selected Make.

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -7,16 +7,23 @@ import { MakeService } from '../../services/make.service';
   styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
-  makes: any;
+  makes: any[] = [];
+  models: any[] = [];
+  vehicle: any = {};
 
   constructor(private makeService: MakeService) { }
 
   ngOnInit() {
     this.makeService.getMakes().subscribe(makes => {
       this.makes = makes;
-      console.log("Makes", this.makes);
     });
 
+  }
+
+  onMakeChange()
+  {
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+    this.models = selectedMake ? selectedMake.models : [];
   }
 
 }
