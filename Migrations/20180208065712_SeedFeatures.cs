e8204ecd9ed767bb2bdce0e817de077e246d81f1
Krzysztof <krzysiekk.gda@gmail.com>
2018-02-08 07:09:21 +0000

    add Feature: Model, Resource, Mapping and Controller with GetFeatures method.

---
+++ Migrations/20180208065712_SeedFeatures.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Airconditioning')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('GPS')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('ABS')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Airbag')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features");
+        }
+    }
+}
